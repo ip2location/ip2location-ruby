6e297d6672c1d95cde88f788a102d7eeb3036624
ip2location <support@ip2location.com>
2023-08-24 15:41:03 +0800

    Update the theme used.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 # Read https://www.sphinx-doc.org/en/master/usage/configuration.html for more options available
 
-import sphinx_pdj_theme
+# import sphinx_pdj_theme
 
 # -- Project information
 
@@ -29,17 +29,27 @@ myst_enable_extensions = [
     "fieldlist",
 ]
 
+# https://myst-parser.readthedocs.io/en/latest/configuration.html#setting-html-metadata
+myst_html_meta = {
+    "description": "IP2Location Ruby library that enables user to get the geolocation information from IP2Location BIN database.",
+    "keywords": "IP2Location, Geolocation, IP location",
+    "google-site-verification": "DeW6mXDyMnMt4i61ZJBNuoADPimo5266DKob7Z7d6i4",
+}
+
 # templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_pdj_theme'
-html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+html_theme = 'sphinx_book_theme'
+# html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
 # PDJ theme options, see the list of available options here: https://github.com/jucacrispim/sphinx_pdj_theme/blob/master/sphinx_pdj_theme/theme.conf
 html_theme_options = {
-    # Hide the name of the project above the search bar
-    'home_link': 'hide'
+    "use_edit_page_button": False,
+    "use_source_button": False,
+    "use_issues_button": False,
+    "use_download_button": False,
+    "use_sidenotes": False,
 }
 
 # The name of an image file (relative to this directory) to place at the top
@@ -49,4 +59,6 @@ html_logo = 'images/ipl-logo-square-1200.png'
 # Favicon
 html_favicon = 'images/favicon.ico'
 
-html_title = "IP2Location Python"
+html_title = "IP2Location Ruby"
+
+html_baseurl = "https://ip2location-ruby.readthedocs.io/en/latest/"
